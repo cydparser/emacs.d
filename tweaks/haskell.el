1fb1950de3e0d52d4c4a7edfcce82ae5c37003d4
cydparser <cydparser@gmail.com>
2013-09-15 18:28:53 -0700

    Add haskell flymake

--- tweaks/haskell.el
+++ tweaks/haskell.el
@@ -1,5 +1,14 @@
-(add-hook 'haskell-mode-hook
-          (lambda ()
-            (define-key haskell-mode-map (kbd "C-x C-s") 'haskell-mode-save-buffer)
-            (turn-on-haskell-doc-mode)
-            (turn-on-haskell-indentation)))
+(init-package-install 'haskell-mode)
+(init-package-install 'flymake-haskell-multi)
+
+(setq haskell-tags-on-save t)
+
+(require 'speedbar)
+(speedbar-add-supported-extension ".hs")
+
+(defun tweak-haskell-mode ()
+  (turn-on-haskell-doc-mode)
+  (turn-on-haskell-indentation)
+  (flymake-haskell-multi-load))
+
+(add-hook 'haskell-mode-hook 'tweak-haskell-mode)
