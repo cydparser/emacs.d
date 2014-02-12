77d26459b74c5431236065ecb86eecdb3612208c
cydparser <cydparser@gmail.com>
2014-02-12 16:39:05 -0700

    Add ruby snippets and tweak ruby-mode

--- tweaks/ruby.el
+++ tweaks/ruby.el
@@ -4,12 +4,20 @@
 (add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Guardfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Vagrantfile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
 
-(autoload 'run-ruby "inf-ruby"
-  "Run an inferior Ruby process")
+(defconst rspec-spec-file-name-re "\\(_\\|-\\)[j]?spec\\.rb\\'"
+  "The regex to identify spec files: /([_-][j]?spec.rb$/")
+
+(defmacro init-rspec-file-p ()
+  (and buffer-file-name
+       (rspec-spec-file-p buffer-file-name)))
+
+(autoload 'run-ruby "inf-ruby")
+(autoload 'ruby-block-mode "ruby-block")
 
 (defun tweak-ruby-mode ()
   (subword-mode 1)
