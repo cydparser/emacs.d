9c30f8404e8723706abe788e15d92d981e540878
cydparser <cydparser@gmail.com>
2014-08-25 17:08:08 -0700

    Add flx-ido

--- tweaks/ido.el
+++ tweaks/ido.el
@@ -1,7 +1,12 @@
+(init-package-require 'flx-ido)
+
 (setq ido-create-new-buffer 'always
       ido-enable-flex-matching t
+      ido-use-faces nil
       ido-everywhere t
       ido-ignore-extensions t
       ido-save-directory-list-file (expand-file-name "ido.last" user-cache-directory))
 
 (ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
