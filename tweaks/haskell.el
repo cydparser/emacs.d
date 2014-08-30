42e391a7af6a4c9ad854c4faacf9fddd4cb9e1e2
cydparser <cydparser@gmail.com>
2014-08-29 18:20:21 -0700

    Fix interactive-haskell-mode

--- tweaks/haskell.el
+++ tweaks/haskell.el
@@ -1,14 +1,15 @@
 (init-package-install 'haskell-mode)
 (init-package-install 'flymake-haskell-multi)
 
-(setq haskell-tags-on-save t)
-
-(require 'speedbar)
-(speedbar-add-supported-extension ".hs")
+(setq haskell-tags-on-save t
+      haskell-process-suggest-remove-import-lines t
+      haskell-process-auto-import-loaded-modules t
+      haskell-process-log t)
 
 (defun tweak-haskell-mode ()
   (turn-on-haskell-doc-mode)
   (turn-on-haskell-indentation)
-  (flymake-haskell-multi-load))
+  (flymake-haskell-multi-load)
+  (interactive-haskell-mode))
 
 (add-hook 'haskell-mode-hook 'tweak-haskell-mode)
