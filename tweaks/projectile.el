8cc8a5af660d74d533294e4d143c61409f9f8b38
cydparser <cydparser@gmail.com>
2014-09-14 22:40:53 -0700

    Add projectile-tags-command path hack

--- tweaks/projectile.el
+++ tweaks/projectile.el
@@ -3,4 +3,7 @@
 (setq projectile-mode-line `(:eval (format " Proj[%s]" (projectile-project-name)))
       projectile-globally-ignored-files `("/log" "/target" "/tmp" "/var"))
 
+;; call-process uses a different path for some reason...
+(setq projectile-tags-command (concat "PATH=" (getenv "PATH") " ctags -Re -f %s %s"))
+
 (projectile-global-mode)
