d02fe88c47a9132c247a347b83145bb713248034
cydparser <cydparser@gmail.com>
2014-12-15 12:52:22 -0800

    Clean up projectile

--- tweaks/projectile.el
+++ tweaks/projectile.el
@@ -1,9 +1,8 @@
 (init-package-install 'projectile)
 
 (setq projectile-mode-line `(:eval (format " Proj[%s]" (projectile-project-name)))
-      projectile-globally-ignored-files `("/log" "/target" "/tmp" "/var"))
-
-;; call-process uses a different path for some reason...
-(setq projectile-tags-command (concat "PATH=" (getenv "PATH") " ctags -Re -f %s %s"))
+      projectile-globally-ignored-files `("/log" "/target" "/tmp" "/var")
+      ;; call-process uses a different path for some reason...
+      projectile-tags-command (concat "PATH=" (getenv "PATH") " ctags -Re -f %s %s"))
 
 (projectile-global-mode)
