fbaa85e3331b47690edb234f533a025cb5472d36
cydparser <cydparser@gmail.com>
2015-01-02 17:19:40 -0800

    Improve haskell tweaks

--- tweaks/haskell.el
+++ tweaks/haskell.el
@@ -1,17 +1,21 @@
+;; Cabal install: happy hasktags hlint stylish-haskell
+
 (init-package-install 'haskell-mode)
 (init-package-install 'flymake-haskell-multi)
 
-(setq haskell-tags-on-save t
-      ;; interactive-haskell-mode
-      haskell-process-suggest-remove-import-lines t
-      haskell-process-auto-import-loaded-modules t
-      haskell-process-log t)
+(setq
+ haskell-tags-on-save t
+ haskell-process-suggest-remove-import-lines t
+ haskell-process-auto-import-loaded-modules t
+ haskell-process-log t)
 
 (defun tweak-haskell-mode ()
   (turn-on-haskell-doc-mode)
   (turn-on-haskell-indentation)
   (flymake-haskell-multi-load)
-  (interactive-haskell-mode))
+  (interactive-haskell-mode)
+  (let ((m haskell-mode-map))
+    (define-key m (kbd "C-c C-p") 'haskell-navigate-imports)))
 
 ;; (autoload 'ghc-init "ghc" nil t)
 ;; (autoload 'ghc-debug "ghc" nil t)
