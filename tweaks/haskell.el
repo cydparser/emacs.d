4b94a9b69b6eb500fa94188ad317018c355acd65
cydparser <cydparser@gmail.com>
2015-01-13 00:37:52 -0800

    Add more haskell tweaks

--- tweaks/haskell.el
+++ tweaks/haskell.el
@@ -1,22 +1,31 @@
-;; Cabal install: happy hasktags hlint stylish-haskell
+;; cabal install happy hasktags hlint present stylish-haskell
+;; https://github.com/serras/emacs-haskell-tutorial/blob/master/tutorial.md
 
 (init-package-install 'haskell-mode)
 (init-package-install 'flymake-haskell-multi)
 
 (setq
  haskell-tags-on-save t
- haskell-process-suggest-remove-import-lines t
  haskell-process-auto-import-loaded-modules t
- haskell-process-log t)
+ haskell-process-log t
+ haskell-process-suggest-remove-import-lines t
+ haskell-process-type 'cabal-repl)
 
 (defun tweak-haskell-mode ()
   (turn-on-haskell-doc-mode)
   (turn-on-haskell-indentation)
   (flymake-haskell-multi-load)
   (interactive-haskell-mode)
-  (let ((m haskell-mode-map))
-    (define-key m (kbd "C-c C-p") 'haskell-navigate-imports)
-    (define-key m (kbd "C-c C-y") 'haskell-mode-stylish-buffer)))
+  (let ((hm haskell-mode-map))
+    ;; (define-key m (kbd "C-c C-l") 'haskell-process-load-or-reload)
+    (define-key hm (kbd "C-c C-n C-c") 'haskell-process-cabal-build)
+    (define-key hm (kbd "C-c C-n C-i") 'haskell-process-do-info)
+    (define-key hm (kbd "C-c C-n C-t") 'haskell-process-do-type)
+    ;; (define-key m (kbd "C-c C-n c") 'haskell-process-cabal)
+    (define-key hm (kbd "C-c C-p") 'haskell-navigate-imports)
+    (define-key hm (kbd "C-c C-y") 'haskell-mode-stylish-buffer)
+    (define-key hm (kbd "C-c C-z") 'haskell-interactive-switch)
+    (define-key hm (kbd "SPC") 'haskell-mode-contextual-space)))
 
 ;; (autoload 'ghc-init "ghc" nil t)
 ;; (autoload 'ghc-debug "ghc" nil t)
@@ -25,3 +34,11 @@
   (ghc-init))
 
 (add-hook 'haskell-mode-hook 'tweak-haskell-mode)
+
+(eval-after-load 'haskell-cabal
+  '(progn
+     (let ((cm haskell-cabal-mode-map))
+       (define-key cm (kbd "C-c C-c") 'haskell-process-cabal-build)
+       (define-key cm (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+       (define-key cm (kbd "C-c C-z") 'haskell-interactive-switch)
+       (define-key cm (kbd "C-c c") 'haskell-process-cabal))))
