e7fbf4b7239616da02ca6aea545b06fc9ed135fa
cydparser <cydparser@gmail.com>
2015-02-23 11:11:52 -0800

    Set shell shebang and make executable

--- tweaks/sh.el
+++ tweaks/sh.el
@@ -1,3 +1,8 @@
 (let ((spaces 2))
   (setq sh-basic-offset spaces
         sh-indentation spaces))
+
+(defun tweak-sh-mode ()
+  (sh-set-shell "/bin/bash" t t))
+
+(add-hook 'sh-mode-hook 'tweak-sh-mode)
