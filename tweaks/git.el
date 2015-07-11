cf677ce58049c3f897d3dacd8316c13aa45b86fb
cydparser <cydparser@gmail.com>
2015-07-11 01:55:22 -0700

    Add "q" binding to magit rebase

--- tweaks/git.el
+++ tweaks/git.el
@@ -1,2 +1,6 @@
 (init-package-install 'git-link)
 (init-package-install 'magit)
+
+(with-eval-after-load 'git-rebase
+  (let ((map git-rebase-mode-map))
+    (define-key map (kbd "q") 'with-editor-finish)))
