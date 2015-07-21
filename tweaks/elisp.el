9f749c070df6c37fc5d8c2b55f70778efd4d1e64
cydparser <cydparser@gmail.com>
2015-07-20 20:00:27 -0700

    Add rainbow-delimiters to elisp-mode

--- tweaks/elisp.el
+++ tweaks/elisp.el
@@ -1,7 +1,9 @@
 (init-package-install 'aggressive-indent)
+(init-package-install 'rainbow-delimiters)
 
 (defun tweak-emacs-lisp-mode ()
   (unless (equal (buffer-name) "*scratch*")
-    (aggressive-indent-mode)))
+    (aggressive-indent-mode)
+    (rainbow-delimiters-mode)))
 
 (add-hook 'emacs-lisp-mode-hook 'tweak-emacs-lisp-mode)
