668a3415374fb29520491cf8ef8717d8876ad894
cydparser <cydparser@gmail.com>
2015-08-12 22:46:50 -0700

    Decrease magit noise

--- init/git.el
+++ init/git.el
@@ -1,5 +1,8 @@
 (defalias 'ms 'magit-status)
 
+(setq magit-push-always-verify nil
+      magit-revert-buffers t)
+
 (with-eval-after-load 'git-rebase
   (let ((map git-rebase-mode-map))
     (define-key map (kbd "q") 'with-editor-finish)))
