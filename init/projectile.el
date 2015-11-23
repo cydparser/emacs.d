cc5e05a9e7e875d55633f9f16f615a9b65743278
cydparser <cydparser@gmail.com>
2015-11-23 15:11:34 -0800

    Disable projectile-mode-line

--- init/projectile.el
+++ init/projectile.el
@@ -1,4 +1,4 @@
-(setq projectile-mode-line `(:eval (format " Proj[%s]" (projectile-project-name)))
+(setq projectile-mode-line nil
       projectile-globally-ignored-files `("/log" "/target" "/tmp" "/var")
       ;; call-process uses a different path for some reason...
       projectile-tags-command (concat "PATH=" (getenv "PATH") " ctags -Re -f %s %s")
