8e41f3e8920054177e448d3bb12bf4e067bb02d3
cydparser <cydparser@gmail.com>
2016-02-12 19:04:01 -0800

    Enable flyspell in git-commit-mode

--- init/git.el
+++ init/git.el
@@ -6,3 +6,8 @@
 (with-eval-after-load 'git-rebase
   (let ((map git-rebase-mode-map))
     (define-key map (kbd "q") 'with-editor-finish)))
+
+(defun init-git-commit-mode-hook ()
+  (flyspell-mode))
+
+(add-hook 'git-commit-mode-hook 'init-git-commit-mode-hook)
