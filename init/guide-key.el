e852adaaed9cac851c5468d9cc2e428314b626a9
cydparser <cydparser@gmail.com>
2016-03-21 12:54:33 -0700

    Add use-package and switch to which-key

--- init/guide-key.el
+++
@@ -1,7 +0,0 @@
-(require 'guide-key)
-
-(setq
- guide-key/guide-key-sequence t
- guide-key/popup-window-position :bottom)
-
-(guide-key-mode 1)
---
+++ init/which-key.el
@@ -0,0 +1,3 @@
+(use-package which-key
+  :init (setq-default which-key-idle-delay 0.5)
+  :config (which-key-mode 1))
