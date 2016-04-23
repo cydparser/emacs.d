a945300c93232bf60c826a646dee2efa9cd0bc96
cydparser <cydparser@gmail.com>
2016-04-22 17:01:01 -0700

    Update melpa URL

--- init.el
+++ init.el
@@ -57,7 +57,7 @@
 
 (require 'package)
 
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 
 (setq package-enable-at-startup nil)
 (package-initialize)
