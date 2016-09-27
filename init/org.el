8a01f4dcfd6b3aaf68608d513addc266f23ca8c4
cydparser <cydparser@gmail.com>
2016-09-27 00:44:41 -0700

    Load common org-babel languages

--- init/org.el
+++ init/org.el
@@ -6,7 +6,15 @@
           org-log-done 'time
           org-src-fontify-natively t
           org-startup-truncated nil)
-    (add-hook 'org-mode-hook #'flyspell-mode)))
+    (add-hook 'org-mode-hook #'flyspell-mode))
+  :config
+  (progn
+    (progn
+      (org-babel-do-load-languages
+       'org-babel-load-languages
+       '((emacs-lisp . t)
+         (ruby . t)
+         (sh . t))))))
 
 (use-package org-bullets
   :defer t
