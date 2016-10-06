9aeace7657a0e0c9f18be5c03e40b7482ad1a15e
cydparser <cydparser@gmail.com>
2016-10-05 20:46:32 -0700

    Use custom projectile-test-suffix-function

--- init/projectile.el
+++ init/projectile.el
@@ -3,12 +3,18 @@
   :diminish ""
   :init
   (progn
+    (defun init-projectile-test-suffix (project-type)
+      "Find default test files suffix based on PROJECT-TYPE."
+      (cond ((member project-type '(haskell-stack)) "Spec")
+            (t (projectile-test-suffix project-type))))
+
     (setq projectile-mode-line nil
           ;; `call-process` uses a different path.
           projectile-tags-command (concat "PATH=" (getenv "PATH") " ctags -Re -f \"%s\" %s")
+          projectile-test-suffix-function #'init-projectile-test-suffix
           projectile-use-git-grep t)
     (make-variable-buffer-local 'projectile-tags-command)
-    (projectile-global-mode)))
+    (projectile-mode)))
 
 (use-package helm-projectile
   :demand
