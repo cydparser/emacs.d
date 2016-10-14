d1ec8b9684218a7d6a25c03c7194d68f1af313ad
cydparser <cydparser@gmail.com>
2016-10-13 22:59:30 -0700

    Set package-selected-packages

--- custom.el
+++ custom.el
@@ -6,6 +6,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(menu-bar-mode nil)
+ '(package-selected-packages
+   (quote
+    (zenburn-theme yaml-mode which-key use-package twittering-mode systemd scss-mode ruby-block rainbow-delimiters org-bullets nix-mode multi-term markdown-mode magit intero hlint-refactor helm-projectile helm-nixos-options helm-descbinds haskell-snippets haml-mode git-link git-gutter floobits exec-path-from-shell erc-hl-nicks dockerfile-mode docker company-quickhelp company-nixos-options company-cabal command-log-mode coffee-mode cmm-mode aggressive-indent)))
  '(ruby-align-to-stmt-keywords t)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
