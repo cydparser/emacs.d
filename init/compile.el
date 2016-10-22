b9359a90b038f88e9b8b0f2cd30d1c5add060b43
cydparser <cydparser@gmail.com>
2016-10-22 16:31:21 -0700

    Simplify init-compile-colorize

--- init/compile.el
+++ init/compile.el
@@ -3,13 +3,10 @@
   :init
   (progn
     (defun init-compile-colorize ()
-      "Colorize compilation.
-http://endlessparentheses.com/ansi-colors-in-the-compilation-buffer-output.html"
-      (let ((inhibit-read-only t)
-            (end (point)))
-        (save-excursion
-          (goto-char compilation-filter-start)
-          (move-beginning-of-line nil)
-          (ansi-color-apply-on-region (point) end))))
+      "Colorize compilation."
+      (let ((inhibit-read-only t))
+        (goto-char compilation-filter-start)
+        (move-beginning-of-line nil)
+        (ansi-color-apply-on-region (point) (point-max))))
 
     (add-hook 'compilation-filter-hook #'init-compile-colorize)))
