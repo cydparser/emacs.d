633f04643449574978e308baf56c12c535cf3ccd
cydparser <cydparser@gmail.com>
2016-11-05 16:19:01 -0700

    Convert flyspell config to use-package

--- init/flyspell.el
+++ init/flyspell.el
@@ -1,21 +1,25 @@
-;; TODO: Convert to use-package
+(use-package ispell
+  :defer t
+  :init
+  (progn
+    (setq ispell-silently-savep t)
+    (let ((pdict (init-xdg-config "ispell/words")))
+      (if (file-exists-p pdict)
+          (setq ispell-personal-dictionary pdict)))))
 
-;; (setq ispell-silently-savep t)
-
-;; (let ((pdict (init-xdg-config "ispell/words")))
-;;   (if (file-exists-p pdict)
-;;       (setq ispell-personal-dictionary pdict)))
-
-;; ;; http://stackoverflow.com/a/22116480/1231408
-;; (defun init-flyspell-save-word ()
-;;   "Add word at point to dictionary."
-;;   (interactive)
-;;   (let ((cursor-location (point))
-;;         (word (flyspell-get-word)))
-;;     (if (consp word)
-;;         (flyspell-do-correct 'save nil (car word) cursor-location (cadr word) (caddr word) cursor-location))))
-
-;; (with-eval-after-load 'flyspell
-;;   (let ((map flyspell-mode-map))
-;;     (define-key map flyspell-auto-correct-binding nil)
-;;     (define-key map (kbd "C-M-.") 'init-flyspell-save-word)))
+(use-package flyspell
+  :defer t
+  :bind (:map flyspell-mode-map
+              ("C-M-." . init-flyspell-save-word))
+  :config
+  (progn
+    (defun init-flyspell-save-word ()
+      "Add word at point to dictionary.
+Source: http://stackoverflow.com/a/22116480/1231408"
+      (interactive)
+      (let ((cursor-location (point))
+            (word (flyspell-get-word)))
+        (if (consp word)
+            (flyspell-do-correct 'save nil (car word)
+                                 cursor-location (cadr word) (caddr word)
+                                 cursor-location))))))
