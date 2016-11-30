37498ec700b25ddace42d3fea613e01ed436faa5
cydparser <cydparser@gmail.com>
2016-11-29 20:43:28 -0800

    Disable whitespace highlighting in compilation-mode

--- init/compile.el
+++ init/compile.el
@@ -4,11 +4,15 @@
   (progn
     (setq compilation-scroll-output 'first-error)
 
-    (defun init-compile-colorize ()
+    (defun init-compilation-mode ()
+      (init-whitespace-disable))
+
+    (defun init-compilation-colorize ()
       "Colorize compilation."
       (let ((inhibit-read-only t))
         (goto-char compilation-filter-start)
         (move-beginning-of-line nil)
         (ansi-color-apply-on-region (point) (point-max))))
 
-    (add-hook 'compilation-filter-hook #'init-compile-colorize)))
+    (add-hook 'compilation-filter-hook #'init-compilation-colorize)
+    (add-hook 'compilation-mode-hook #'init-compilation-mode)))
