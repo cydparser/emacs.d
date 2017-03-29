ec7e6f926b9f07916bf8b2dfc21a6248201fd3fc
cydparser <cydparser@gmail.com>
2017-03-28 23:31:12 -0700

    Bind SPC to widget-button-press in widget-keymap

--- init/widget.el
+++ init/widget.el
@@ -2,6 +2,7 @@
   :defer t
   :ensure nil
   :bind (:map widget-keymap
+              ("SPC" . widget-button-press)
               ("n" . next-line)
               ("p" . previous-line)
               ("q" . init-kill-buffer-current)))
