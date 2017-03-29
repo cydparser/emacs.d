ae72b889e8db00014ea4a074691327f80e7c1a22
cydparser <cydparser@gmail.com>
2017-03-28 23:32:47 -0700

    Configure bash on windows

--- init/windows.el
+++ init/windows.el
@@ -1,6 +1,13 @@
 (defconst init-os-win (and (memq window-system '(pc w32)) t)
   "True when running on Windows.")
 
+(when init-os-win
+  (setq explicit-bash.exe-args '("--noediting" "--login" "-i")
+        explicit-shell-file-name "C:/msys64/usr/bin/bash.exe"
+        shell-file-name "bash")
+  (setenv "SHELL" shell-file-name)
+  (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m))
+
 (use-package powershell
   :defer t
   :if init-os-win)
