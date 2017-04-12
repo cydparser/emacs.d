14d542c3c0286f953c5f54fdf6cefdb7a2e33b5b
cydparser <cydparser@gmail.com>
2017-04-11 19:01:03 -0700

    Change smerge-command-prefix

--- custom.el
+++ custom.el
@@ -11,6 +11,7 @@
     (zenburn-theme yaml-mode which-key web-mode use-package twittering-mode systemd sdcv scss-mode ruby-block rainbow-mode rainbow-delimiters powershell org-tree-slide org-bullets nix-mode multi-term markdown-mode magit intero imenu-anywhere hlint-refactor helm-projectile helm-nixos-options helm-descbinds haskell-snippets haml-mode git-link git-gutter flycheck-pos-tip floobits fancy-narrow expand-region exec-path-from-shell erc-hl-nicks dumb-jump dockerfile-mode docker demo-it company-quickhelp company-nixos-options company-cabal command-log-mode coffee-mode cmm-mode aggressive-indent)))
  '(ruby-align-to-stmt-keywords t)
  '(scroll-bar-mode nil)
+ '(smerge-command-prefix "m")
  '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
