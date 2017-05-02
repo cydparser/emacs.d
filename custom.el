74e9fe00c464d0860626cb4ae89c0d5d6b767626
cydparser <cydparser@gmail.com>
2017-05-02 16:08:30 -0700

    Update custom package-selected-packages

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
-    (zenburn-theme yaml-mode which-key web-mode use-package twittering-mode systemd sdcv scss-mode ruby-block rainbow-mode rainbow-delimiters powershell org-tree-slide org-bullets nix-mode multi-term markdown-mode magit intero imenu-anywhere hlint-refactor helm-projectile helm-nixos-options helm-descbinds haskell-snippets haml-mode git-link git-gutter flycheck-pos-tip floobits fancy-narrow expand-region exec-path-from-shell erc-hl-nicks dumb-jump dockerfile-mode docker demo-it company-quickhelp company-nixos-options company-cabal command-log-mode coffee-mode cmm-mode aggressive-indent)))
+    (zenburn-theme yaml-mode which-key web-mode use-package twittering-mode systemd solarized-theme sdcv scss-mode ruby-block repl-toggle rainbow-mode rainbow-delimiters powershell org-tree-slide org-bullets nodejs-repl nix-mode multi-term markdown-mode magit intero inf-ruby imenu-anywhere hlint-refactor helm-projectile helm-nixos-options helm-descbinds haskell-snippets haml-mode git-link git-gutter fancy-narrow expand-region exec-path-from-shell erc-services erc-hl-nicks elisp-slime-nav dumb-jump dockerfile-mode docker demo-it dante company-quickhelp company-nixos-options company-cabal command-log-mode coffee-mode cmm-mode auctex aggressive-indent)))
  '(ruby-align-to-stmt-keywords t)
  '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc))))
  '(scroll-bar-mode nil)
