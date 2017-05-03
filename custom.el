683786abd1d06ccf54a113020408a2eb486aea17
cydparser <cydparser@gmail.com>
2017-05-02 21:28:05 -0700

    Add safe local variable intero-stack-executable

--- custom.el
+++ custom.el
@@ -10,7 +10,10 @@
    (quote
     (zenburn-theme yaml-mode which-key web-mode use-package twittering-mode systemd solarized-theme sdcv scss-mode ruby-block repl-toggle rainbow-mode rainbow-delimiters powershell org-tree-slide org-bullets nodejs-repl nix-mode multi-term markdown-mode magit intero inf-ruby imenu-anywhere hlint-refactor helm-projectile helm-nixos-options helm-descbinds haskell-snippets haml-mode git-link git-gutter fancy-narrow expand-region exec-path-from-shell erc-services erc-hl-nicks elisp-slime-nav dumb-jump dockerfile-mode docker demo-it dante company-quickhelp company-nixos-options company-cabal command-log-mode coffee-mode cmm-mode auctex aggressive-indent)))
  '(ruby-align-to-stmt-keywords t)
- '(safe-local-variable-values (quote ((flycheck-disabled-checkers emacs-lisp-checkdoc))))
+ '(safe-local-variable-values
+   (quote
+    ((flycheck-disabled-checkers emacs-lisp-checkdoc)
+     (intero-stack-executable . "intero-nix-shim"))))
  '(scroll-bar-mode nil)
  '(smerge-command-prefix "m")
  '(tool-bar-mode nil))
