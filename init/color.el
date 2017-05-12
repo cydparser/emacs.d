d2bfbab9b60a73112d32c55fdbbc064326915d65
cydparser <cydparser@gmail.com>
2017-05-11 19:48:49 -0700

    Fix zenburn color changes

--- init/color.el
+++ init/color.el
@@ -8,11 +8,13 @@
     (load-theme 'zenburn 'no-confirm))
   :config
   (progn
-    (set-face-bold 'font-lock-keyword-face nil)
     (zenburn-with-color-variables
-      (set-face-attribute 'font-lock-builtin-face nil :foreground zenburn-green+3 :bold nil)
-      (set-face-attribute 'ivy-current-match nil :foreground zenburn-cyan :background zenburn-bg-1 :weight 'normal :underline nil)
-      (set-face-attribute 'ivy-minibuffer-match-face-1 nil :background zenburn-blue-5)
-      (set-face-attribute 'ivy-minibuffer-match-face-2 nil :background zenburn-blue-4)
-      (set-face-attribute 'ivy-minibuffer-match-face-3 nil :background zenburn-blue-3)
-      (set-face-attribute 'ivy-minibuffer-match-face-4 nil :background zenburn-blue-2))))
+      (custom-theme-set-faces
+       'zenburn
+       `(font-lock-builtin-face ((t (:foreground ,zenburn-green+3 :weight normal))))
+       `(font-lock-keyword-face ((t (:foreground ,zenburn-yellow :weight normal))))
+       `(ivy-current-match ((t (:foreground ,zenburn-cyan :background ,zenburn-bg-1 :weight normal :underline nil))))
+       `(ivy-minibuffer-match-face-1 ((t (:background ,zenburn-blue-5))))
+       `(ivy-minibuffer-match-face-2 ((t (:background ,zenburn-blue-4))))
+       `(ivy-minibuffer-match-face-3 ((t (:background ,zenburn-blue-3))))
+       `(ivy-minibuffer-match-face-4 ((t (:background ,zenburn-blue-2))))))))
