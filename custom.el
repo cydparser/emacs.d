1fb5e32b6383cdea41e790a92e0e33c0e150296e
cydparser <cydparser@gmail.com>
2017-05-13 22:54:19 -0700

    Use single frame with ediff

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(menu-bar-mode nil)
  '(package-selected-packages
    (quote
