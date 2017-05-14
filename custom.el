91141a97b1eff56e4c413677f2aba35ed5dfcea9
cydparser <cydparser@gmail.com>
2017-05-13 22:54:35 -0700

    Rearrange (automatic) custom

--- custom.el
+++ custom.el
@@ -13,9 +13,14 @@
  '(ruby-align-to-stmt-keywords t)
  '(safe-local-variable-values
    (quote
-    ((flycheck-disabled-checkers emacs-lisp-checkdoc)
-     (intero-stack-executable . "intero-nix-shim")
+    ((intero-stack-executable . "intero-nix-shim")
      (flycheck-disabled-checkers emacs-lisp-checkdoc))))
  '(scroll-bar-mode nil)
  '(smerge-command-prefix "m")
  '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
