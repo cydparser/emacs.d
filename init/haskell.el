072eb1066ee8e747c15df847be4f5c4cdf4192e3
cydparser <cydparser@gmail.com>
2017-05-14 19:19:58 -0700

    Use init-haskell to start backend mode

--- init/haskell.el
+++ init/haskell.el
@@ -2,25 +2,6 @@
 
 ;; stack install apply-refact codex hasktags hlint
 
-(defvar init--haskell-backend :intero
-  "Backend to use for realtime errors, completions, etc.")
-
-(defun init-haskell-backend-hook (keyword)
-  "Convert KEYWORD symbol into a symbol beginning with 'init-'."
-  (intern (concat "init-" (substring (symbol-name keyword) 1 nil))))
-
-(defun init-haskell-backend-change (backend)
-  "Switch Haskell backend.
-
-This only affects new buffers."
-  (interactive
-   (let ((s (completing-read "Import: " '("dante" "intero") nil t)))
-     (list (intern (concat ":" s)))))
-  (when (not (eq backend init--haskell-backend))
-    (remove-hook 'haskell-mode-hook (init-haskell-backend-hook init--haskell-backend))
-    (add-hook 'haskell-mode-hook (init-haskell-backend-hook backend))
-    (setq init--haskell-backend backend)))
-
 (use-package cmm-mode
   :defer t)
 
@@ -40,14 +21,7 @@ This only affects new buffers."
               ("C-c C-i" . dante-info)
               ("C-c C-r" . dante-auto-fix)
               ("C-c C-t" . dante-type-at))
-  :init
-  (progn
-    (defun init-dante ()
-      (dante-mode)
-      (flycheck-mode))
-
-    (when (eq :dante init--haskell-backend)
-      (add-hook 'haskell-mode-hook #'init-dante)))
+  :init (defalias 'init-dante 'dante-mode)
   :config
   (progn
     (flycheck-add-next-checker 'haskell-dante '(warning . haskell-hlint))
@@ -57,7 +31,7 @@ This only affects new buffers."
 
 (use-package haskell-mode
   :defer t
-  :diminish " λ"
+  :diminish (haskell-mode . " λ")
   :bind (:map haskell-mode-map
               ("M-g i" . haskell-navigate-imports)
               ("M-g M-i" . haskell-navigate-imports))
@@ -76,13 +50,30 @@ This only affects new buffers."
           "cd %s && stack build --ghc-options -ferror-spans"
           haskell-compile-command
           "stack ghc -- -Wall -ferror-spans -fforce-recomp -c %s")
+    (defvar init-haskell-backend-function 'init-intero)
+
+    (defun init-haskell-change-backend (backend)
+      "Change Haskell backend for future buffers."
+      (interactive (list (completing-read "Import: " '("dante" "intero" "") nil t)))
+      (setq init-haskell-backend-function
+            (and backend
+                 (not (string-blank-p backend))
+                 (intern (concat "init-" backend)))))
 
     (defun init-haskell ()
-      (setq company-dabbrev-downcase nil
-            company-dabbrev-ignore-case :ignore-case)
-      (set (make-local-variable 'projectile-tags-command) "codex update"))
+      (cond ((and buffer-file-name
+                  (string-match "^\\(/nix\\|.+/\\.\\(cabal\\|stack\\|stack-work\\)\\)/.+"
+                                buffer-file-name))
+             (eldoc-mode -1)
+             (flycheck-mode -1))
+            (t
+             (setq company-dabbrev-downcase nil
+                   company-dabbrev-ignore-case :ignore-case)
+             (set (make-local-variable 'projectile-tags-command) "codex update")
+             (when (fboundp init-haskell-backend-function)
+               (funcall init-haskell-backend-function)))))
 
-    (add-hook 'haskell-mode #'init-haskell))
+    (add-hook 'haskell-mode-hook #'init-haskell))
   :config
   (progn
     (remove-hook 'haskell-mode-hook #'interactive-haskell-mode)))
@@ -101,20 +92,7 @@ This only affects new buffers."
   :bind (:map intero-mode-map
               ("C-c r i" . init-intero-add-import)
               ("M-." . init-intero-goto-definition))
-  :init
-  (progn
-    (defun init-intero ()
-      "Enable Intero unless visiting a cached dependency."
-      (if (and buffer-file-name
-               (string-match "^\\(/nix\\|.+/\\.\\(cabal\\|stack\\|stack-work\\)\\)/.+"
-                             buffer-file-name))
-          (progn
-            (eldoc-mode -1)
-            (flycheck-mode -1))
-        (intero-mode)))
-
-    (when (eq :intero init--haskell-backend)
-      (add-hook 'haskell-mode-hook #'init-intero)))
+  :init (defalias 'init-intero 'intero-mode)
   :config
   (progn
     (defun init-intero-insert-import ()
