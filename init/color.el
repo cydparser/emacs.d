bfc2abe82c1cc25c0981aadfa3d3cfd10354cdc7
cydparser <cydparser@gmail.com>
2017-05-18 23:03:35 -0700

    Change ivy match face to yellow

--- init/color.el
+++ init/color.el
@@ -13,7 +13,7 @@
        'zenburn
        `(font-lock-builtin-face ((t (:foreground ,zenburn-green+3 :weight normal))))
        `(font-lock-keyword-face ((t (:foreground ,zenburn-yellow :weight normal))))
-       `(ivy-current-match ((t (:foreground ,zenburn-cyan :background ,zenburn-bg-1 :weight normal :underline nil))))
+       `(ivy-current-match ((t (:foreground ,zenburn-yellow :background ,zenburn-bg-1 :weight normal :underline nil))))
        `(ivy-minibuffer-match-face-1 ((t (:background ,zenburn-blue-5))))
        `(ivy-minibuffer-match-face-2 ((t (:background ,zenburn-blue-4))))
        `(ivy-minibuffer-match-face-3 ((t (:background ,zenburn-blue-3))))
