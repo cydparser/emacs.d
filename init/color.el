981178b37aa121e62beaf4edcc8a4a9ae55da68f
cydparser <cydparser@gmail.com>
2017-06-24 19:47:26 -0700

    Move zenburn face customizations into function

--- init/color.el
+++ init/color.el
@@ -7,16 +7,18 @@
   :demand
   :init
   (progn
-    (load-theme 'zenburn 'no-confirm))
-  :config
-  (progn
-    (zenburn-with-color-variables
-      (custom-theme-set-faces
-       'zenburn
-       `(font-lock-builtin-face ((t (:foreground ,zenburn-green+3 :weight normal))))
-       `(font-lock-keyword-face ((t (:foreground ,zenburn-yellow :weight normal))))
-       `(ivy-current-match ((t (:foreground ,zenburn-yellow :background ,zenburn-bg-1 :weight normal :underline nil))))
-       `(ivy-minibuffer-match-face-1 ((t (:background ,zenburn-blue-5))))
-       `(ivy-minibuffer-match-face-2 ((t (:background ,zenburn-blue-4))))
-       `(ivy-minibuffer-match-face-3 ((t (:background ,zenburn-blue-3))))
-       `(ivy-minibuffer-match-face-4 ((t (:background ,zenburn-blue-2))))))))
+    (defun init-load-theme-zenburn ()
+      (interactive)
+      (load-theme 'zenburn 'no-confirm)
+      (zenburn-with-color-variables
+        (custom-theme-set-faces
+         'zenburn
+         `(font-lock-builtin-face ((t (:foreground ,zenburn-green+3 :weight normal))))
+         `(font-lock-keyword-face ((t (:foreground ,zenburn-yellow :weight normal))))
+         `(ivy-current-match ((t (:foreground ,zenburn-yellow :background ,zenburn-bg-1 :weight normal :underline nil))))
+         `(ivy-minibuffer-match-face-1 ((t (:background ,zenburn-blue-5))))
+         `(ivy-minibuffer-match-face-2 ((t (:background ,zenburn-blue-4))))
+         `(ivy-minibuffer-match-face-3 ((t (:background ,zenburn-blue-3))))
+         `(ivy-minibuffer-match-face-4 ((t (:background ,zenburn-blue-2)))))))
+
+    (add-hook 'after-init-hook #'init-load-theme-zenburn)))
