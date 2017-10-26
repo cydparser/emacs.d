68bc987a736d2efe29cafe7e98cabacaa8cfceff
cydparser <cydparser@gmail.com>
2017-10-25 17:11:24 -0700

    Set apropos-sort-by-scores

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(apropos-sort-by-scores t)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(flycheck-disabled-checkers (quote (haskell-ghc haskell-stack-ghc)))
  '(menu-bar-mode nil)
