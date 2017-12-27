4c290a726d7a41fe40b0e950fb11669c6e812f2c
cydparser <cydparser@gmail.com>
2017-12-26 22:01:28 -0800

    Change toggle-repl binding

--- init/repl.el
+++ init/repl.el
@@ -3,7 +3,7 @@
 (use-package repl-toggle
   :diminish ""
   :bind (:map repl-toggle-mode-map
-              ("C-c r" . rtog/toggle-repl))
+              ("C-c t r" . rtog/toggle-repl))
   :hook (prog-mode-hook . rtog/activate)
   :init (setq rtog/goto-buffer-fun #'pop-to-buffer
               rtog/mode-repl-alist '((emacs-lisp-mode . ielm)
