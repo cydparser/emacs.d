0dec0ca9648f2c09e7a9c7e1337f1ba2bca19f06
cydparser <cydparser@gmail.com>
2018-02-17 18:39:46 -0800

    Bind 'n' and 'p' in compilation mode

--- init/compile.el
+++ init/compile.el
@@ -2,10 +2,14 @@
 
 (use-package compile
   :ensure nil
+  :bind (:map compilation-mode-map
+              ("n" . next-error)
+              ("p" . previous-error))
   :hook (compilation-filter-hook . init-compilation-colorize)
-  :init (setq compilation-scroll-output 'first-error)
   :init
   (progn
+    (setq compilation-scroll-output 'first-error)
+
     (defun init-compilation-colorize ()
       "Colorize compilation."
       (let ((inhibit-read-only t))
