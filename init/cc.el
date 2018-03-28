90062ba3c7360a402fe9bc2228c6463b847cced0
cydparser <cydparser@gmail.com>
2018-03-27 22:04:12 -0700

    Adjust java indentation

--- init/cc.el
+++ init/cc.el
@@ -8,4 +8,7 @@
     (defun init-java ()
       (setq c-basic-offset 4
             tab-stop-list (number-sequence 4 fill-column 4)
-            tab-width 4))))
+            tab-width 4)
+      (c-set-offset 'arglist-close 0)
+      (c-set-offset 'arglist-intro '+)
+      (c-set-offset 'case-label '+))))
