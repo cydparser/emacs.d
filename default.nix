1bb27c551fcc08520036543b77b7511c66e7eab3
cydparser <cydparser@gmail.com>
2018-05-20 23:56:07 -0700

    Add default.nix (wip)

---
+++ default.nix
@@ -0,0 +1,64 @@
+{ stdenv, fetchurl, unzip
+, ctags
+, emacs
+, espeak
+, haskellPackages
+, hlint
+, hunspell
+, hunspellDicts
+, libxml2
+, nixpkgs-lint
+, ruby
+, sdcv
+, shellcheck
+, wordnet
+}:
+let
+  inherit (haskellPackages) apply-refact hasktags codex;
+
+  flycheck-yaml-ruby = ruby;
+
+  hunspellDict = hunspellDicts.en-us;
+
+  mwebster-1913 = stdenv.mkDerivation {
+    name = "merriam-webster-1913";
+
+    sourceRoot = ".";
+
+    src = fetchurl {
+      url = "https://s3.amazonaws.com/jsomers/dictionary.zip";
+      sha256 = "09y673q5v46ps72pnm1jz0jx2bcyfbsmzw3f2x9y9s99k3yf79ps";
+    };
+
+    nativeBuildInputs = [ unzip ];
+
+    installPhase = ''
+      mkdir -p "$out/dic"
+      tar -C "$out/dic" -xf dictionary/stardict-dictd-web1913-2.4.2.tar.bz2
+    '';
+  };
+
+  xmllint = libxml2.bin;
+
+in stdenv.mkDerivation {
+  name = "emacs.d";
+
+  buildInputs = [
+    apply-refact
+    codex
+    flycheck-yaml-ruby
+    hasktags
+    hlint
+    hunspellDict
+    shellcheck
+    xmllint
+    mwebster-1913
+  ];
+
+  postInstall = ''
+    wrapProgram ${emacs}/bin/emacs \
+      --suffix-each PATH : "${shellcheck}/bin" \
+      --set DICPATH "${hunspellDict}/share/hunspell" \
+      --set STARDICT_DATA_DIR ${mwebster-1913}
+  '';
+}
