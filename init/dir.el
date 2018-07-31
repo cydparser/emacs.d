4655085678896c757e3f4c1b3eeaecc84667e661
cydparser <cydparser@gmail.com>
2018-07-31 15:12:52 -0700

    Set wdired-allow-to-change-permissions to true

--- init/dir.el
+++ init/dir.el
@@ -13,3 +13,8 @@
 (use-package treemacs-projectile
   :after projectile
   :bind (("C-c g t" . treemacs-projectile-toggle)))
+
+;; Type C-x C-q in a dired buffer.
+(use-package wdired
+  :ensure nil
+  :init (setq wdired-allow-to-change-permissions t))
