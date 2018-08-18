14462b848d10605dbf52a7b2a3e9e0881f26f918
cydparser <cydparser@gmail.com>
2018-08-17 22:06:16 -0700

    Add string-inflection

--- init/prog.el
+++ init/prog.el
@@ -15,3 +15,10 @@
 
     (defun init-prog-mode ()
       (init-prettify-symbols))))
+
+(use-package string-inflection
+  :bind (("C-c r c -" . string-inflection-kebab-case)      ; kebab-case
+         ("C-c r c c" . string-inflection-camelcase)       ; CamelCase
+         ("C-c r c l" . string-inflection-lower-camelcase) ; lowerCamelCase
+         ("C-c r c s" . string-inflection-underscore)      ; snake_case
+         ("C-c r c u" . string-inflection-upcase)))        ; UPPER_SNAKE_CASE
