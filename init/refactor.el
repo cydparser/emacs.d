f32cfdeaa1ba690a760320f67fc12705ad308755
cydparser <cydparser@gmail.com>
2018-08-25 23:07:18 -0700

    Fix iedit C-o binding

--- init/refactor.el
+++ init/refactor.el
@@ -1,10 +1,7 @@
+;;; -*- lexical-binding: t -*-
+
 (use-package iedit
+  :demand
   :diminish "🙾"
-  :bind (("C-;" . iedit-mode)
-         :map esc-map
-         ("C-;" . iedit-execute-last-modification)
-         :map iedit-mode-keymap
-         ("C-o" . iedit-toggle-unmatched-lines-visible)
-         :map isearch-mode-map
-         ("C-;" . iedit-mode-from-isearch))
-  :init (setq iedit-toggle-key-default nil))
+  :bind (:map iedit-mode-keymap
+              ("C-o" . iedit-show/hide-unmatched-lines)))
