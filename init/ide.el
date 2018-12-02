3bd2f563bbdbba47e9ed7b4224835d564f5f5f59
cydparser <cydparser@gmail.com>
2018-12-02 00:20:00 -0800

    Move company into ide.el

--- init/ide.el
+++ init/ide.el
@@ -6,6 +6,25 @@
   :after flycheck
   :bind (("C-c r f" . attrap-attrap)))
 
+(use-package company
+  :demand
+  :diminish ""
+  :bind ("M-C-/" . company-complete)
+  :hook (after-init-hook . global-company-mode)
+  :init
+  (progn
+    (setq company-backends '(company-capf
+                             company-files
+                             (company-dabbrev-code company-etags company-keywords)
+                             company-dabbrev))
+    (setq company-idle-delay 0.3)
+    (setq-default company-dabbrev-downcase nil
+                  company-dabbrev-ignore-case nil)))
+
+(use-package company-quickhelp
+  :demand
+  :hook (company-mode-hook . company-quickhelp-mode))
+
 (use-package flycheck
   :demand
   :diminish ""
