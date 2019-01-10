ecd37111c3be3ae87055484ced33d944eb01ee01
cydparser <cydparser@gmail.com>
2019-01-09 21:45:25 -0800

    Adjust Java indentation

--- init/cc.el
+++ init/cc.el
@@ -2,13 +2,20 @@
 
 (use-package cc-mode
   :ensure nil
-  :hook (c-mode-common-hook . init-cc)
+  :hook ((c-mode-common-hook . init-cc)
+         (java-mode-hook . init-java))
   :init
   (progn
     (require 'google-c-style)
 
     (defun init-cc ()
-      (c-add-style "Google" google-c-style t))))
+      (c-add-style "Google" google-c-style t))
+
+    (defun init-java ()
+      (c-set-offset 'arglist-cont 0)
+      (c-set-offset 'arglist-cont-nonempty '++)
+      (c-set-offset 'arglist-intro '++)
+      (c-set-offset 'arglist-close 0))))
 
 (use-package cmake-mode)
 
