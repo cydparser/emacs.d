5776fafc7aa3813327081bde8d66586316d01afe
cydparser <cydparser@gmail.com>
2019-02-06 21:32:00 -0800

    Customize hl-todo font

--- custom.el
+++ custom.el
@@ -26,4 +26,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(hl-todo ((t (:foreground "#cc9393" :weight semi-bold)))))
