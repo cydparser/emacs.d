1b6b67d6ebe346949e095c6f4e5415f06e462af4
cydparser <cydparser@gmail.com>
2019-02-15 23:04:25 -0800

    Set delete-old-versions

--- custom.el
+++ custom.el
@@ -6,6 +6,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(apropos-sort-by-scores t)
+ '(delete-old-versions t)
  '(ediff-window-setup-function (quote ediff-setup-windows-plain))
  '(flycheck-disabled-checkers (quote (haskell-ghc haskell-stack-ghc)))
  '(large-file-warning-threshold 524288000)
