868bb219a69b556fee046758bb806218be34e2e5
cydparser <cydparser@gmail.com>
2019-02-24 20:05:44 -0800

    Add hlint

--- default.nix
+++ default.nix
@@ -6,12 +6,18 @@ let
 
   fetchPinnedUrl = path: builtins.fetchurl (builtins.fromJSON (builtins.readFile path));
 
-  hs = pkgs.haskellPackages;
+  hs = pkgs.haskellPackages.extend (_super: self: {
+    haskell-src-exts = self.callPackage nix/haskell-src-exts.nix {};
+
+    repline = hs.callPackage nix/repline.nix {};
+  });
+
+  skipTests = pkgs.haskell.lib.dontCheck;
 in
 rec {
   codex = hs.callCabal2nix "codex" (fetchPinnedGitHub nix/codex.json) {};
 
-  dhall = pkgs.haskell.lib.dontCheck (hs.callPackage nix/dhall.nix { megaparsec = hs.megaparsec_7_0_0; inherit repline; });
+  dhall = skipTests (hs.callPackage nix/dhall.nix { megaparsec = hs.megaparsec_7_0_0; });
 
   emacs = pkgs.emacsWithPackages (epkgs: with epkgs.melpaStablePackages; [
     epkgs.pdf-tools
@@ -21,13 +27,24 @@ rec {
 
   hies = callPackage nix/hies.nix { inherit fetchPinnedGitHub; };
 
+  hlint = hs.callPackage nix/hlint.nix {};
+
   jdt-language-server = callPackage nix/jdt-language-server.nix { jre = pkgs.jre10_headless; inherit fetchPinnedUrl; };
 
   nix-linter = (import (fetchPinnedGitHub nix/nix-linter.json) { inherit pkgs; }).nix-linter;
 
   mwebster-1913 = callPackage nix/mwebster-1913.nix {};
 
-  mx = callPackage nix/mx.nix { inherit codex dhall emacs hasktags hies jdt-language-server nix-linter mwebster-1913; };
-
-  repline = hs.callPackage nix/repline.nix {};
+  mx = callPackage nix/mx.nix { inherit
+    codex
+    dhall
+    emacs
+    hasktags
+    hies
+    hlint
+    jdt-language-server
+    nix-linter
+    mwebster-1913
+    ;
+  };
 }
