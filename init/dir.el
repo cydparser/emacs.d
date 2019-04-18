b035e119fad2d2b496690fd4f56ce413c7af1676
cydparser <cydparser@gmail.com>
2019-04-17 22:15:13 -0700

    Switch from treemacs to dired-subtree

--- init/dir.el
+++ init/dir.el
@@ -1,5 +1,24 @@
 ;;; -*- lexical-binding: t -*-
 
+(use-package dired-subtree
+  :bind (:map dired-mode-map
+              ("i" . dired-subtree-insert)
+              ;; ("k" . dired-subtree-remove)
+              ("C-d" . dired-subtree-remove)
+              ;; ("TAB" . dired-subtree-toggle)
+              ("TAB" . dired-subtree-cycle)
+              ;; dired-subtree-revert
+              ;; dired-subtree-narrow
+              ("C-M-u" . dired-subtree-up)
+              ("C-M-d" . dired-subtree-down)
+              ;; dired-subtree-next-sibling
+              ;; dired-subtree-previous-sibling
+              ;; dired-subtree-beginning
+              ;; dired-subtree-end
+              ;; dired-subtree-unmark-subtree
+              ;; dired-subtree-only-this-directory
+              ))
+
 (use-package dired-x
   :ensure nil
   :hook (dired-load-hook . init-dired-load)
@@ -7,12 +26,6 @@
   (progn
     (defun init-dired-load ()
       (load "dired-x"))))
-
-(use-package treemacs)
-
-(use-package treemacs-projectile
-  :after projectile
-  :bind (("C-c g t" . treemacs-projectile-toggle)))
 
 ;; Type C-x C-q in a dired buffer.
 (use-package wdired
