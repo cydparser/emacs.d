c56bffd0a8d90cabbbc4b216d2c70747f89bf4de
cydparser <cydparser@gmail.com>
2019-04-17 22:15:43 -0700

    Set dumb-jump-force-searcher to 'rg

--- init/dumb-jump.el
+++ init/dumb-jump.el
@@ -4,7 +4,8 @@
   :bind (("C-c j j" . dumb-jump-go)
          ("C-c j p" . dumb-jump-back)
          ("C-c j l" . dumb-jump-quick-look))
-  :init (setq dumb-jump-selector 'ivy)
+  :init (setq dumb-jump-force-searcher 'rg
+              dumb-jump-selector 'ivy)
   :config
   (progn
     (unbind-key "C-M-g" dumb-jump-mode-map)
