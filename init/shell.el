5549e53abbbcc7ac08efa8fa2cd9623a137ff762
cydparser <cydparser@gmail.com>
2019-04-17 22:18:08 -0700

    Add direnv

--- init/shell.el
+++ init/shell.el
@@ -5,6 +5,9 @@
   :bind (:map projectile-command-map
               ("x b" . better-shell-for-projectile-root)))
 
+(use-package direnv
+  :hook (after-init-hook . direnv-mode))
+
 (use-package shell
   :ensure nil
   :bind (:map shell-mode-map
