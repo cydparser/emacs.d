cee236b4f6a09aff87c62fcdd0a891cbb59df835
cydparser <cydparser@gmail.com>
2019-04-22 23:42:32 -0700

    Enable history file in shell

--- init/shell.el
+++ init/shell.el
@@ -1,5 +1,7 @@
 ;;; -*- lexical-binding: t -*-
 
+(require 'init-utils)
+
 (use-package better-shell
   :after projectile
   :bind (:map projectile-command-map
@@ -11,4 +13,11 @@
 (use-package shell
   :ensure nil
   :bind (:map shell-mode-map
-              ("SPC" . comint-magic-space)))
+              ("SPC" . comint-magic-space))
+  :init
+  (progn
+    (let ((hist (init-xdg-data "zsh/history")))
+      (when (and (file-exists-p hist)
+                 (not (getenv "HISTFILE")))
+        (setenv "HISTFILE" hist)
+        (setenv "HISTSIZE" "4096")))))
