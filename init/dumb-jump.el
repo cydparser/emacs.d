d4dbde621fd6f6aeb0b861d908aaa0fdf656defb
cydparser <cydparser@gmail.com>
2019-09-16 22:19:49 -0700

    Improve dumb-jump rules

--- init/dumb-jump.el
+++ init/dumb-jump.el
@@ -2,6 +2,7 @@
 
 (use-package dumb-jump
   :bind (("C-c j j" . dumb-jump-go)
+         ("C-c j 4 j" . dumb-jump-go-other-window)
          ("C-c j p" . dumb-jump-back)
          ("C-c j l" . dumb-jump-quick-look))
   :init (setq dumb-jump-force-searcher 'rg
@@ -18,23 +19,23 @@
            'list
            '((:type "top-level binding" :supports ("rg") :language "haskell"
                     :regex "^JJJ($|(\\s+(::|([^:=|-]*(= |[|])))))")
-             (:type "local binding" :supports ("rg") :language "haskell"
-                    :regex "^\\s+((let|where)\\s+)?JJJ($|(\\s+(::|([^:=|-]*(= |[|])))))")
+             ;; (:type "local binding" :supports ("rg") :language "haskell"
+             ;;        :regex "^\\s+((let|where)\\s+)?JJJ($|(\\s+(::|([^:=|-]*(= |[|])))))")
              (:type "data constructor" :supports ("rg") :language "haskell"
                     :regex "^(data|newtype)\\s+.*(((= |[|])\\s+JJJ($|\\s)))")
              (:type "data constructor newline" :supports ("rg") :language "haskell"
                     :regex "^\\s+[=|]\\s+(forall\\s+[^.]+\\.\\s+)?JJJ(\\s|$)")
              (:type "record field" :supports ("rg") :language "haskell"
-                    :regex "^((data|newtype)\\s+.*((= |[|])\\s+[^{]+[{]\\s+JJJ\\s+::))|(\\s+[,{]\\s+JJJ\\s+::)")
+                    :regex "^(((data|newtype)\\s+.*((= |[|])\\s+[^{]+[{]\\s+JJJ\\s+::))|(\\s+[,{]\\s+JJJ\\s+::))")
              (:type "GADT constructor" :supports ("rg") :language "haskell"
                     :regex "^\\s+JJJ\\s+::\\s")
              (:type "module" :supports ("rg") :language "haskell"
                     :regex "^module\\s+JJJ(\\s|$)")
              (:type "type-like" :supports ("rg") :language "haskell"
-                    :regex "^((data(\\s+family)?)|(newtype)|(type(\\s+family)?))\\s+JJJ(\\s|$)")
+                    :regex "^((data(\\s+family)?)|newtype|(type(\\s+family)?))\\s+JJJ(\\s|$)")
              (:type "typeclass" :supports ("rg") :language "haskell"
                     :regex "^class\\s+([^=]+=>\\s+)?JJJ(\\s|$)")
              (:type "associated type family" :supports ("rg") :language "haskell"
-                    :regex "^\\s+type\\s+Key\\s+[^:]+::"))
+                    :regex "^\\s+type\\s+JJJ\\s+[^:]+::"))
            (seq-remove (lambda (rule) (string-equal "haskell" (plist-get rule :language)))
                        dumb-jump-find-rules)))))
