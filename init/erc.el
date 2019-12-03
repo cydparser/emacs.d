b98090619439cb050ad6fc239979e08a787a1968
cydparser <cydparser@gmail.com>
2019-12-02 17:58:07 -0800

    Partially fix init-erc-tls

--- init/erc.el
+++ init/erc.el
@@ -14,11 +14,8 @@
     (let ((ca (init-xdg-config "ssl/local-ca.pem"))
           (cert (init-xdg-config "irc/local.pem")))
       (when (and (file-exists-p ca) (file-exists-p cert))
-        ;; Disable the built-in gnutls: it does not use tls-program.
-        (defun gnutls-available-p () nil)
-
         (setq tls-program (list (concat "openssl s_client -connect %h:%p"
-                                        " -no_ssl2 -ign_eof"
+                                        " -ign_eof"
                                         " -CAfile " ca " -cert " cert)
                                 "gnutls-cli --x509cafile %t -p %p %h"))))
     (defun init-erc-mode ()
