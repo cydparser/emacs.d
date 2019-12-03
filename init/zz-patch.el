54c1350e60bca346c862132449a4f5d091e65a93
cydparser <cydparser@gmail.com>
2019-12-02 16:57:26 -0800

    Fix init-patch

--- init/zz-patch.el
+++ init/zz-patch.el
@@ -3,9 +3,14 @@
 (defmacro init-patch (package broken-version &rest body)
   "Apply BODY until PACKAGE is past BROKEN-VERSION."
   (declare (indent defun))
-  `(if (version-list-<= ,broken-version (pkg-info-package-version ,package))
-       (with-eval-after-load (symbol-name ,package) ,@body)
-     (message "Package %s may no longer be broken" package)))
+  `(let ((desc (alist-get ,package package-alist)))
+     (cond
+      ((null desc)
+       (message "Package not found: %s" ,package))
+      ((version-list-<= ,broken-version (package-desc-version (car desc)))
+       (with-eval-after-load (symbol-name ,package)
+         ,@body))
+      (t (message "Package %s may no longer be broken" ,package)))))
 
 (init-patch 'counsel '(20190624 1444)
   (defun counsel-rg (&optional initial-input initial-directory extra-rg-args rg-prompt)
