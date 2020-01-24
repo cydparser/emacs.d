38c86485930b6a6ac1427da9d18a6dfa245db97a
cydparser <cydparser@gmail.com>
2020-01-23 21:20:26 -0800

    Add bindings to outshine-mode-map

--- init/literate.el
+++ init/literate.el
@@ -6,6 +6,9 @@
   :diminish ((outline-minor-mode . "٭")
              (outshine-mode . " "))
   :hook ((outline-minor-mode-hook . outshine-mode)
-         (prog-mode-hook . outline-minor-mode)))
+         (prog-mode-hook . outline-minor-mode))
+  :bind (:map outshine-mode-map
+              ("C-c C-n" . outline-next-visible-heading)
+              ("C-c C-p" . outline-previous-visible-heading)))
 
 (use-package poporg)
