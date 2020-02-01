388e078562b3b017660844a53a33a8e1f73ca11e
cydparser <cydparser@gmail.com>
2020-01-31 18:59:56 -0800

    Remove outshine from hooks

--- init/literate.el
+++ init/literate.el
@@ -5,8 +5,6 @@
 (use-package outshine
   :diminish ((outline-minor-mode . "٭")
              (outshine-mode . " "))
-  :hook ((outline-minor-mode-hook . outshine-mode)
-         (prog-mode-hook . outline-minor-mode))
   :bind (:map outshine-mode-map
               ("C-c C-n" . outline-next-visible-heading)
               ("C-c C-p" . outline-previous-visible-heading)))
