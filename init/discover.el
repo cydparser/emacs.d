7759c49800cb2ac8e405cfb4e590bdb1603ff0a6
cydparser <cydparser@gmail.com>
2020-02-15 15:32:24 -0800

    Add ace-link

--- init/discover.el
+++ init/discover.el
@@ -1,5 +1,9 @@
 ;;; -*- lexical-binding: t -*-
 
+(use-package ace-link
+  :demand
+  :hook (after-init-hook . ace-link-setup-default))
+
 (use-package info-colors
   :hook (Info-selection-hook . info-colors-fontify-node))
 
