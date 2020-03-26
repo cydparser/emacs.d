5a59cfaedffe057f789bf75879c0d8236cd9d387
cydparser <cydparser@gmail.com>
2020-03-25 19:01:30 -0700

    Fix dumb-jump regex's

--- init/dumb-jump.el
+++ init/dumb-jump.el
@@ -18,21 +18,23 @@
           (seq-concatenate
            'list
            '((:type "top-level binding" :supports ("rg") :language "haskell"
-                    :regex "^JJJ($|(\\s+(::|([^:=|-]*(= |[|])))))")
+                    :regex "^JJJ($|(\\s+(::|([^:=|-]*(=|[|])))))")
              ;; (:type "local binding" :supports ("rg") :language "haskell"
              ;;        :regex "^\\s+((let|where)\\s+)?JJJ($|(\\s+(::|([^:=|-]*(= |[|])))))")
+             (:type "type" :supports ("rg") :language "haskell"
+                    :regex "^type\\s+(family\\s+)?JJJ")
              (:type "data constructor" :supports ("rg") :language "haskell"
-                    :regex "^(data|newtype)\\s+.*(((= |[|])\\s+JJJ($|\\s)))")
+                    :regex "^(data|newtype)\\s+.*(((=|[|])\\s+JJJ($|\\s)))")
              (:type "data constructor newline" :supports ("rg") :language "haskell"
                     :regex "^\\s+[=|]\\s+(forall\\s+[^.]+\\.\\s+)?JJJ(\\s|$)")
              (:type "record field" :supports ("rg") :language "haskell"
-                    :regex "^(((data|newtype)\\s+.*((= |[|])\\s+[^{]+[{]\\s+JJJ\\s+::))|(\\s+[,{]\\s+JJJ\\s+::))")
+                    :regex "^(((data|newtype)\\s+.*((=|[|])\\s+[^{]+[{]\\s+JJJ\\s+::))|(\\s+[,{]\\s+JJJ\\s+::))")
              (:type "GADT constructor" :supports ("rg") :language "haskell"
                     :regex "^\\s+JJJ\\s+::\\s")
              (:type "module" :supports ("rg") :language "haskell"
-                    :regex "^module\\s+JJJ(\\s|$)")
-             (:type "type-like" :supports ("rg") :language "haskell"
-                    :regex "^((data(\\s+family)?)|newtype|(type(\\s+family)?))\\s+JJJ(\\s|$)")
+                    :regex "^module\\s+JJJ(\\s|$)"
+                    :tests ("module Test (exportA, exportB) where"
+                            "module Test"))
              (:type "typeclass" :supports ("rg") :language "haskell"
                     :regex "^class\\s+([^=]+=>\\s+)?JJJ(\\s|$)")
              (:type "associated type family" :supports ("rg") :language "haskell"
