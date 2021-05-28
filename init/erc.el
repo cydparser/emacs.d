e5638d3ed7481877d7fb8631f6341147997a3b12
cydparser <cydparser@gmail.com>
2021-05-27 22:05:29 -0700

    Update erc

--- init/erc.el
+++ init/erc.el
@@ -11,19 +11,19 @@
           erc-track-enable-keybindings nil
           erc-track-when-inactive t)
 
-    (let ((ca (init-xdg-config "ssl/local-ca.pem"))
-          (cert (init-xdg-config "irc/local.pem")))
-      (when (and (file-exists-p ca) (file-exists-p cert))
-        (setq tls-program (list (concat "openssl s_client -connect %h:%p"
-                                        " -ign_eof"
-                                        " -CAfile " ca " -cert " cert)
-                                "gnutls-cli --x509cafile %t -p %p %h"))))
+    ;; (let ((ca (init-xdg-config "ssl/local-ca.pem"))
+    ;;       (cert (init-xdg-config "irc/local.pem")))
+    ;;   (when (and (file-exists-p ca) (file-exists-p cert))
+    ;;     (setq tls-program (list (concat "openssl s_client -connect %h:%p"
+    ;;                                     " -ign_eof"
+    ;;                                     " -CAfile " ca " -cert " cert)
+    ;;                             "gnutls-cli --x509cafile %t -p %p %h"))))
     (defun init-erc-mode ()
       (erc-spelling-mode 1)
       (erc-track-mode))
 
     (defun init-erc-tls (&optional server port)
-      (interactive '("irc.freenode.net" 7000))
+      (interactive '("irc://irc.libera.chat" 6667))
       (erc-tls :server server :port port))))
 
 (use-package erc-hl-nicks
