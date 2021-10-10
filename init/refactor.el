5e4fc41be4c7af887cc210068ee255799463ad60
cydparser <cydparser@gmail.com>
2021-10-09 20:49:34 -0700

    Remove "C-:"

--- init/refactor.el
+++ init/refactor.el
@@ -9,17 +9,8 @@
               ("<return>" . iedit--quit)))
 
 (use-package multiple-cursors
-  :bind (("C-:" . init-multiple-cursors-edit)
-         ("<M-S-down>" . mc/mark-next-lines)
+  :bind (("<M-S-down>" . mc/mark-next-lines)
          ("<M-S-up>" . mc/mark-previous-lines)
          ("<M-down-mouse-1>" . mc/toggle-cursor-on-click))
-  :init
-  (progn
-    (setq mc/list-file (expand-file-name "multiple-cursors.el" init-config-directory)))
-  :config
-  (progn
-    (defun init-multiple-cursors-edit ()
-      (interactive)
-      (if (region-active-p)
-          (mc/edit-lines)
-        (mc/mark-all-like-this-dwim nil)))))
+  :custom
+  (mc/list-file (expand-file-name "multiple-cursors.el" init-config-directory)))
