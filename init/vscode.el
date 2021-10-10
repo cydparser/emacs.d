3f444b5ff184ae73c5c5bb67e3b97282133eea44
cydparser <cydparser@gmail.com>
2021-10-09 21:34:39 -0700

    Add vscode bindings

---
+++ init/vscode.el
@@ -0,0 +1,12 @@
+;;; -*- lexical-binding: t -*-
+
+(defun init-scroll-down-line ()
+  (interactive)
+  (scroll-down 1))
+
+(defun init-scroll-up-line ()
+  (interactive)
+  (scroll-up 1))
+
+(bind-key "<C-down>" #'init-scroll-up-line)
+(bind-key "<C-up>" #'init-scroll-down-line)
