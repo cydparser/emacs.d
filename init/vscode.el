8730eed09f1f9cfc82e9bded39717ca27e3537ce
cydparser <cydparser@gmail.com>
2022-06-13 19:48:01 -0700

    Bind <C-M-S-down> & <C-M-S-up> to copy line

--- init/vscode.el
+++ init/vscode.el
@@ -12,3 +12,33 @@
 (bind-key "<C-up>" #'init-scroll-down-line)
 
 (bind-key "<M-S-down-mouse-1>" #'mouse-drag-region-rectangle)
+
+(defun init-copy-line (&optional up)
+  (interactive)
+  (let ((p (point))
+        (orig-goal-column goal-column)
+        (line (string-trim-right (thing-at-point 'line t) "[\n\r]+")))
+    (cond
+     ((and (eq p (buffer-end 1)) (eq p (point-at-bol)))
+      (if up (open-line 1) (newline 1))
+      )
+     (up
+      (beginning-of-line)
+      (open-line 1)
+      (insert line)
+      (goto-char p))
+     (t
+      (setq goal-column (current-column))
+      (end-of-line)
+      (newline 1)
+      (insert line)
+      (goto-char p)
+      (next-line 1)
+      (setq goal-column orig-goal-column)))))
+
+(defun init-copy-line-up ()
+  (interactive)
+  (init-copy-line :up))
+
+(bind-key "<C-M-S-down>" #'init-copy-line)
+(bind-key "<C-M-S-up>"   #'init-copy-line-up)
