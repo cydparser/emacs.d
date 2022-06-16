440efa2edc135dbf0f79b9ec4724249a0e19b44f
cydparser <cydparser@gmail.com>
2022-06-15 22:45:56 -0700

    Remove lsp-java

--- init/cc.el
+++ init/cc.el
@@ -17,19 +17,3 @@
       (c-set-offset 'arglist-close 0))))
 
 (use-package cmake-mode)
-
-(use-package lsp-java
-  :ensure nil
-  :after lsp-mode
-  :init
-  (progn
-    (let ((workspace (expand-file-name "lsp-java/" init-var-directory)))
-      (setq lsp-java-format-settings-profile "Google"
-            lsp-java-format-settings-url "https://raw.githubusercontent.com/google/styleguide/gh-pages/eclipse-java-google-style.xml"
-            lsp-java-import-gradle-enabled nil
-            lsp-java-completion-import-order '("com" "java" "javax" "org")
-            lsp-java-workspace-cache-dir (expand-file-name "cache/" workspace)
-            lsp-java-workspace-dir workspace)))
-
-  (with-eval-after-load "lsp-mode"
-    (require 'lsp-java)))
