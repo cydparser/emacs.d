1623baae74c02a887e186b8489229c3b2cb675c8
cydparser <cydparser@gmail.com>
2022-06-23 19:15:14 -0700

    Change smartparens bindings

--- init/paren.el
+++ init/paren.el
@@ -4,11 +4,13 @@
   :diminish "$"
   :bind (("C-w" . init-sp-kill-region-or-backward-word)
          :map smartparens-mode-map
-         ("C-S-K" . sp-kill-whole-line)
+         ("C-S-c" . sp-convolute-sexp)
+         ("C-S-i" . sp-splice-sexp)
+         ("C-S-j" . sp-join-sexp)
+         ("C-S-k" . sp-kill-whole-line)
+         ("C-S-r" . sp-splice-sexp-killing-around)
+         ("C-S-s" . sp-split-sexp)
          ("C-k" . sp-kill-hybrid-sexp)
-         ("M-J" . sp-join-sexp)
-         ("M-N" . sp-next-sexp)
-         ("M-P" . sp-previous-sexp)
          (")" . sp-up-sexp))
   :hook (((org-mode-hook text-mode-hook) . turn-on-smartparens-mode)
          (prog-mode-hook . turn-on-smartparens-mode))
@@ -28,6 +30,10 @@
     (unbind-key "<M-down>" smartparens-mode-map)
     (unbind-key "<M-up>" smartparens-mode-map)
     (unbind-key "M-?" smartparens-mode-map)
+    (unbind-key "M-S" smartparens-mode-map)
+    (unbind-key "M-j" smartparens-mode-map)
+    (unbind-key "M-r" smartparens-mode-map)
+    (unbind-key "M-s" smartparens-mode-map)
 
     (defun init-sp-kill-region-or-backward-word (arg)
       "Kill selected region or backward word."
