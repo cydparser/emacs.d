95be74b59622247fbfb10637cd54fa845132616f
cydparser <cydparser@gmail.com>
2022-08-05 17:22:53 -0700

    Show git submodules in magit-status

--- init/git.el
+++ init/git.el
@@ -12,4 +12,10 @@
 (use-package magit
   :hook (git-commit-mode-hook . flyspell-mode)
   :init (setq magit-push-always-verify nil
-              magit-revert-buffers t))
+              magit-revert-buffers t)
+  :custom
+  (magit-module-sections-nested nil)
+  :config
+  (progn
+    (magit-add-section-hook 'magit-status-sections-hook
+                            'magit-insert-modules nil :append)))
