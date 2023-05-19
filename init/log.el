0ac337a5cf621d114f97a6358582e863a054d37c
cydparser <cydparser@gmail.com>
2023-05-18 18:38:47 -0700

    Convert logview config to custom

--- init/log.el
+++ init/log.el
@@ -3,17 +3,14 @@
 (require 'init-utils)
 
 (use-package logview
-  :init
-  (progn
-    (setq logview-cache-filename (expand-file-name "logview-cache.extmap" init-var-directory)
-          logview-views-file (expand-file-name "logview.views" init-config-directory))
-
-    (setq logview-additional-timestamp-formats
-          '(("ISO 8601 datetime + pico + time zone" (java-pattern . "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSSSS+0000"))))
-
-    (setq logview-additional-submodes
-          '(("Tripshot" . ((format . "TIMESTAMP LEVEL (ThreadId THREAD) NAME :")
-                           (levels . "SLF4J"))))))
+  :custom
+  (logview-additional-submodes
+   '(("TripShot" . ((format . "TIMESTAMP LEVEL (ThreadId THREAD) NAME :")
+                    (levels . "SLF4J")))))
+  (logview-additional-timestamp-formats
+   '(("ISO 8601 datetime + pico + time zone" (java-pattern . "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSSSS+0000"))))
+  (logview-cache-filename (expand-file-name "logview-cache.extmap" init-var-directory))
+  (logview-views-file (expand-file-name "logview.views" init-config-directory))
   :config
   (progn
     (when (member "Iosevka Term" (font-family-list))
