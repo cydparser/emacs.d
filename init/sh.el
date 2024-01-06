dc19bad036ad7a94142f2098ddc8fc280f3c74d0
cydparser <cydparser@gmail.com>
2024-01-05 22:22:55 -0800

    Delete init-sh

--- init/sh.el
+++ init/sh.el
@@ -2,14 +2,8 @@
 
 (use-package sh-script
   :ensure nil
-  :hook (sh-mode-hook . init-sh)
   :custom
   (sh-basic-offset 2)
-  :init
-  (progn
-    (defun init-sh ()
-      (unless (file-exists-p buffer-file-name)
-        (sh-set-shell "/usr/bin/env bash" t t))))
   :config
   (progn
     (unbind-key "C-c C-f" sh-mode-map)))
