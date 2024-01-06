8bdfa0043c50a5d9bec38cd309eb08d1fb96050e
cydparser <cydparser@gmail.com>
2024-01-05 22:11:58 -0800

    Delete widget.el

--- init/widget.el
+++
@@ -1,9 +0,0 @@
-;;; -*- lexical-binding: t -*-
-
-(use-package widget-edit
-  :ensure nil
-  :bind (:map widget-keymap
-              ("SPC" . widget-button-press)
-              ("n" . next-line)
-              ("p" . previous-line)
-              ("q" . init-kill-buffer-current)))
