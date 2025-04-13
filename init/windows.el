22cd21d5667eb4cdbf89f01b478a48ee33e52e3a
cydparser <cydparser@gmail.com>
2025-04-12 21:37:28 -0700

    Tidy windows conf

--- init/windows.el
+++ init/windows.el
@@ -1,13 +1,14 @@
 ;;; -*- lexical-binding: t -*-
 
-(defconst init-os-win (and (memq window-system '(pc w32)) t)
+(defconst init-os-win (memq window-system '(pc w32))
   "True when running on Windows.")
 
 (when init-os-win
-  (setq explicit-bash.exe-args '("--noediting" "--login" "-i")
+  (setq explicit-bash-args '("--noediting" "--login" "-i")
         explicit-shell-file-name "C:/msys64/usr/bin/bash.exe"
         shell-file-name "bash")
   (setenv "SHELL" shell-file-name)
   (add-hook 'comint-output-filter-functions 'comint-strip-ctrl-m))
 
-(use-package powershell)
+(use-package powershell
+  :if init-os-win)
