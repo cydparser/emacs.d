ea74ca5071eefaffae1d46d01fe63a29c76c49a4
cydparser <cydparser@gmail.com>
2025-06-03 16:29:37 -0700

    Remove unused log format

--- init/log.el
+++ init/log.el
@@ -4,11 +4,6 @@
 
 (use-package logview
   :custom
-  (logview-additional-submodes
-   '(("TripShot" . ((format . "TIMESTAMP LEVEL (ThreadId THREAD) NAME :")
-                    (levels . "SLF4J")))))
-  (logview-additional-timestamp-formats
-   '(("ISO 8601 datetime + pico + time zone" (java-pattern . "yyyy-MM-dd'T'HH:mm:ss.SSSSSSSSSSSS+0000"))))
   (logview-cache-filename (expand-file-name "logview-cache.extmap" init-var-directory))
   (logview-views-file (expand-file-name "logview.views" init-config-directory))
   :config
