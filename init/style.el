bfe1f745fbe3102d0de08b64f362a50faa1364f5
cydparser <cydparser@gmail.com>
2025-06-04 16:43:32 -0700

    Remove unicode-fonts

--- init/style.el
+++ init/style.el
@@ -22,26 +22,6 @@
 (use-package rainbow-mode
   :diminish (rainbow-mode . "🌈"))
 
-(use-package unicode-fonts
-  :hook (after-init-hook . unicode-fonts-setup)
-  :custom
-  (unicode-fonts-fallback-font-list '("Symbola" "Cascadia Code"))
-  (unicode-fonts-ignore-overrides t)
-  :config
-  (progn
-    (setq-default unicode-fonts-block-font-mapping
-                  (seq-map (lambda (pair)
-                             (list (car pair)
-                                   (let ((fonts (cadr pair)))
-                                     (or (seq-mapcat (lambda (font)
-                                                       (cond ((string-match-p "^Quivira" font) (list "Cascadia Code"))
-                                                             ((string-match-p "^\\(Cascadia\\|Noto \\|Symbola\\)" font) (list font))
-                                                             (t nil))
-                                                       )
-                                                     fonts)
-                                         fonts))))
-                           unicode-fonts-block-font-mapping))))
-
 (use-package xterm-color
   :init
   (progn
