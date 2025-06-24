e91c2a88343cdd3426df8a4cf3c99a11e993d6c5
cydparser <cydparser@gmail.com>
2025-06-24 15:59:58 -0700

    Move markup languages

--- init/markup.el
+++ init/markup.el
@@ -1,3 +1,23 @@
 ;;; -*- lexical-binding: t -*-
 
+(use-package auctex)
+
 (use-package markdown-mode)
+
+(use-package nxml-mode
+  :ensure nil
+  :hook (web-mode-hook . init-nxml)
+  :init
+  (progn
+    (defun init-nxml ()
+      (add-to-list (make-local-variable 'company-backends) #'company-nxml)))
+  :config
+  (progn
+    (unbind-key "C-c C-f" nxml-mode-map)))
+
+(use-package tex
+  :ensure nil
+  :custom
+  (TeX-auto-save t)
+  (TeX-newline-function 'newline-and-indent)
+  (TeX-parse-self t))
