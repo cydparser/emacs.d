fef867fceba277de2123d8972c317e10a62d26f1
cydparser <cydparser@gmail.com>
2025-07-25 15:30:21 -0700

    Remove command-log-mode

--- init/command-log.el
+++
@@ -1,7 +0,0 @@
-;;; -*- lexical-binding: t -*-
-
-(use-package command-log-mode
-  :diminish (command-log-mode . "log")
-  :custom
-  (command-log-mode-is-global t)
-  (command-log-mode-key-binding-open-log "C-c l"))
