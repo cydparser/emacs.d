02aae9501edc3f584c9817b5a2771fa985535ba1
cydparser <cydparser@gmail.com>
2025-07-25 15:32:50 -0700

    Move imenu-anywhere

--- init/imenu.el
+++
@@ -1,4 +0,0 @@
-;;; -*- lexical-binding: t -*-
-
-(use-package imenu-anywhere
-  :bind ("C-c g m" . ivy-imenu-anywhere))
--- init/navigation.el
+++ init/navigation.el
@@ -2,3 +2,6 @@
 
 (use-package ace-window
   :bind (("M-o" . ace-window)))
+
+(use-package imenu-anywhere
+  :bind ("C-c g m" . ivy-imenu-anywhere))
