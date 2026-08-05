77c1fec8d3334d640627b9d630fe24e6f5f76275
cydparser <cydparser@gmail.com>
2026-08-04 17:24:14 -0700

    Move init-patch into init-utils

--- lib/init-utils.el
+++ lib/init-utils.el
@@ -40,6 +40,18 @@
      (when (file-exists-p file)
        (,@sexp file))))
 
+(defmacro init-patch (package broken-version &rest body)
+  "Apply BODY until PACKAGE is past BROKEN-VERSION."
+  (declare (indent defun))
+  `(let ((desc (alist-get ,package package-alist)))
+     (cond
+      ((null desc)
+       (message "Package not found: %s" ,package))
+      ((version-list-<= ,broken-version (package-desc-version (car desc)))
+       (with-eval-after-load (symbol-name ,package)
+         ,@body))
+      (t (message "Package %s may no longer be broken" ,package)))))
+
 (require 'treesit)
 
 (defun init-treesit-print-node (&optional parser-or-lang)
