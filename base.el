42515b4b3f465acfbabd6fd2fa7d523788ecea2c
cydparser <cydparser@gmail.com>
2026-08-05 17:28:08 -0700

    Unbind "M-o" in ibuffer-mode-map

--- base.el
+++ base.el
@@ -133,7 +133,6 @@ ARG determines the direction and number of sexps."
 (bind-key "C-h C-k" #'find-function-on-key)
 (bind-key "C-h C-v" #'find-variable-at-point)
 (bind-key "C-w" #'init-kill-region-or-backward-word)
-(bind-key "C-x C-b" #'ibuffer)
 (bind-key "C-x k" #'init-kill-buffer-current)
 (bind-key "M-/" #'hippie-expand)
 (bind-key "M-L" #'init-sexp-downcase)
@@ -366,6 +365,13 @@ ARG determines the direction and number of sexps."
 
 (use-package hydra
   :demand)
+
+(use-package ibuffer
+  :ensure nil
+  :bind (("C-x C-b" . ibuffer))
+  :config
+  (progn
+    (unbind-key "M-o" ibuffer-mode-map)))
 
 (use-package isearch
   :ensure nil
